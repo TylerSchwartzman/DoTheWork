0d8c0ddecbb2c843df9971ba535b5fca693523bd
Tyler <tschwartzman.dev@gmail.com>
2021-04-07 21:16:15 -0500

    Create Flow class file and FlowTests file

---
+++ DoTheWork/Flow.swift
@@ -0,0 +1,8 @@
+//
+//  Flow.swift
+//  DoTheWork
+//
+//  Created by Tyler Schwartzman on 4/7/21.
+//
+
+import Foundation
---
+++ DoTheWorkTests/FlowTests.swift
@@ -0,0 +1,8 @@
+//
+//  FlowTests.swift
+//  DoTheWorkTests
+//
+//  Created by Tyler Schwartzman on 4/7/21.
+//
+
+import Foundation
