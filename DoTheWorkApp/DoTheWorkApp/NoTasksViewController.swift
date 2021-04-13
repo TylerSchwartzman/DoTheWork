9cf0227e4b720be1074c42e141d51509a0cda766
Tyler <tschwartzman.dev@gmail.com>
2021-04-13 13:29:27 -0500

    Create 'NoTasksViewController' and 'NoTasksViewControllerTests' files

---
+++ DoTheWorkApp/DoTheWorkApp/NoTasksViewController.swift
@@ -0,0 +1,8 @@
+//
+//  NoTasksViewController.swift
+//  DoTheWorkApp
+//
+//  Created by Tyler Schwartzman on 4/13/21.
+//
+
+import Foundation
---
+++ DoTheWorkApp/DoTheWorkAppTests/NoTasksViewControllerTest.swift
@@ -0,0 +1,12 @@
+//
+//  NoTasksViewController.swift
+//  DoTheWorkAppTests
+//
+//  Created by Tyler Schwartzman on 4/13/21.
+//
+
+import UIKit
+
+class NoTasksViewController: UIViewController {
+    
+}
