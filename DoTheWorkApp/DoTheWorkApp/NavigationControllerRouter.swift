030a0187483382e17bdc3c619814dfdd53a3588f
Tyler <tschwartzman.dev@gmail.com>
2021-04-28 21:03:06 -0500

    Add NavigationControllerRouter and NavigationControllerRouterTests files

---
+++ DoTheWorkApp/DoTheWorkApp/NavigationControllerRouter.swift
@@ -0,0 +1,8 @@
+//
+//  NavigationControllerRouter.swift
+//  DoTheWorkApp
+//
+//  Created by Tyler Schwartzman on 4/16/21.
+//
+
+import Foundation
---
+++ DoTheWorkApp/DoTheWorkAppTests/NavigationControllerRouterTest.swift
@@ -0,0 +1,8 @@
+//
+//  NavigationControllerRouterTest.swift
+//  DoTheWorkAppTests
+//
+//  Created by Tyler Schwartzman on 4/16/21.
+//
+
+import Foundation
